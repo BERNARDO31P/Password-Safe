da88cc83bbff7a4816869c3904092960291c0b34
Bernardo de Oliveira <bernardo@bernardo.fm>
2023-04-06 11:44:56 +0200

    Improved query params

--- ch/brueggli/ui/app/src/assets/js/shared.service.ts
+++ ch/brueggli/ui/app/src/assets/js/shared.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 import {User} from "./model/User";
 import {Organization} from "./model/Organization";
@@ -20,13 +20,23 @@ export class SharedService {
 
   organizations: Array<Organization> = [];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.queryParams.subscribe(params => {
       if (params["page"]) this.page = Number(params["page"]);
       if (params["sort"]) this.sorting.sort = params["sort"];
       if (params["order"]) this.sorting.order = params["order"];
 
-      if (this.search === "" && params["search"]) this.page = 1;
+      if (this.search === "" && params["search"]) {
+        this.page = 1;
+        this.router.navigate(
+          [],
+          {
+            relativeTo: this.route,
+            queryParams: {page: null},
+            queryParamsHandling: "merge"
+          });
+      }
+
       this.search = params["search"] ?? "";
     });
   }
